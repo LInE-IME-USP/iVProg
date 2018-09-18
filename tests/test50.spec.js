e4245eadb9d1044da9136b83295b28f039f29d0a
Lucas de Souza <lucascalion@gmail.com>
2018-09-18 11:22:38 -0300

    Fix findFunction in semanticAnalyser
    Fix missing ArrayAccess  expression in semanticAnalyser
    
    Implement test cases for semantic analysis

--- tests/test50.spec.js
+++ tests/test50.spec.js
@@ -2,14 +2,13 @@ import { IVProgParser } from './../js/ast/ivprogParser';
 import { SemanticAnalyser } from './../js/processor/semantic/semanticAnalyser';
 import { LanguageService } from '../js/services/languageService';
 
-describe('A valid code', function () {
+describe('The semantic analyser', function () {
 
   const code = `programa {
 
     funcao inicio() {
-      real a
-      leia(a)
-      a = a + 0xff
+      inteiro a[5] = {1, 2, 3, 4, 0xff}
+      escreva(a[6])
     }
   }`;
 
@@ -17,7 +16,7 @@ describe('A valid code', function () {
 
   const lexer = LanguageService.getCurrentLexer();
 
-  it(`should not throw a semantic error`, function () {
+  it(`should not check when accessing a vector/matrix for size, only types`, function () {
     const parser = new IVProgParser(code, lexer);
     const sem = new SemanticAnalyser(parser.parseTree());
     const fun = sem.analyseTree.bind(sem);
