d1e1a4cafd6ff8bfe39f1d4b84c92a1e89830825
Lucas de Souza <lucascalion@gmail.com>
2019-01-07 20:13:09 -0200

    Implement type system adjusment to  enable implicit casting

--- js/typeSystem/type.js
+++ js/typeSystem/type.js
@@ -1,3 +1,7 @@
+import { Config } from "../util/config";
+import { Types } from "./types";
+import { BaseTypes } from "./baseTypes";
+
 export class Type {
 
   constructor(baseType) {
@@ -18,7 +22,16 @@ export class Type {
 
   isCompatible (another) {
     if(another instanceof Type) {
-      return this.baseType.isCompatible(another.baseType);
+      let result = this.baseType.isCompatible(another.baseType);
+      if(result) {
+        return true;
+      } else if(Config.enable_type_casting) {
+        if (this.baseType === BaseTypes.INTEGER || this.baseType === BaseTypes.REAL) {
+          if (another.baseType === BaseTypes.INTEGER || another.baseType === BaseTypes.REAL) {
+            return true;
+          }
+        }
+      }
     }
     return false;
   }
